e7968399d0256ffa88fe8668f14d2e944eac6db6
Y.N <kazumiya50@hotmail.com>
2013-09-19 23:42:42 +0900

    コマンドライン引数の解析にoptparseを利用するように変更

--- wTrTool.rb
+++ wTrTool.rb
@@ -4,12 +4,20 @@
 ### wTrTool.rb
 ###
 require 'pp'
+require 'optparse'
+opt = OptionParser.new
 
-filename = ARGV[1]
+filename = "MemTrace.dat"
+format = false
+
+opt.on('-f VAL') {|v| filename = v }
+opt.on('-u') {|v| format = v }
+
+argv = opt.parse(ARGV)
 
 binary = File.binread(filename)
 
-if ARGV[0] == "u"
+if format== true
  puts binary.unpack("C*")
 else
  puts binary.unpack("c*")
