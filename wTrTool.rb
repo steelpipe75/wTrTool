8b91e61ff4a28ebc887cffca493138540ca8065e
Y.N <kazumiya50@hotmail.com>
2013-10-20 21:41:37 +0900

    メソッド呼び出しに変更

--- wTrTool.rb
+++ wTrTool.rb
@@ -27,14 +27,14 @@ require 'kwalify'
 
 Version = "v1.1"
 
-inputfilename = "MemTrace.dat"
-outputfilename = "MemTool.txt"
-formatfilename = "wTrToolFormat.yaml"
-patternname = "sample"
-pattern = nil
-endian = "little"
+$inputfilename = "MemTrace.dat"
+$outputfilename = "MemTool.txt"
+$formatfilename = "wTrToolFormat.yaml"
+$patternname = "sample"
+$pattern = nil
+$endian = "little"
 
-format_str = { 
+$FORMAT_STR = { 
   "UINT8"   => {"little" => "C", "big" => "C", "pack" =>"C", "unpack" =>"C", "sprintf" => "%d",  },
   "UINT16"  => {"little" => "v", "big" => "n", "pack" =>"v", "unpack" =>"S", "sprintf" => "%d",  },
   "UINT32"  => {"little" => "V", "big" => "N", "pack" =>"V", "unpack" =>"I", "sprintf" => "%d",  },
@@ -55,9 +55,9 @@ format_str = {
   "DUMMY32" => {"little" => "V", "big" => "N", "pack" =>"V", "unpack" =>"i", "sprintf" => "",    },
 }
 
-dummy = ["DUMMY8", "DUMMY16", "DUMMY32"]
+$DUMMY = ["DUMMY8", "DUMMY16", "DUMMY32"]
 
-schema_def = <<EOS
+$SCHEMA_DEF = <<EOS
 type: seq
 sequence:
   - type: map
@@ -97,122 +97,135 @@ sequence:
                   - DUMMY32
 EOS
 
+$yaml_data = nil
+
 # option parser
+def option_parse
+  opt = OptionParser.new
+  opt.on('-i inputfile',  '--input inputfile',    '入力ファイル指定') { |v| $inputfilename = v }
+  opt.on('-o outputfile', '--output outputfile',  '出力ファイル指定') { |v| $outputfilename = v }
+  opt.on('-f formatfile', '--format formatfile',  '整形パターン記述ファイル指定') { |v| $formatfilename = v }
+  opt.on('-p patternname','--pattern patternname','整形パターン名指定') { |v| $patternname = v }
+  opt.on('-l',            '--littleend',          '入力ファイルの多バイトデータをlittle endianとして扱う') { $endian = "little" }
+  opt.on('-b',            '--bigend',             '入力ファイルの多バイトデータをbig endianとして扱う') { $endian = "big" }
 
-opt = OptionParser.new
-opt.on('-i inputfile',  '--input inputfile',    '入力ファイル指定') { |v| inputfilename = v }
-opt.on('-o outputfile', '--output outputfile',  '出力ファイル指定') { |v| outputfilename = v }
-opt.on('-f formatfile', '--format formatfile',  '整形パターン記述ファイル指定') { |v| formatfilename = v }
-opt.on('-p patternname','--pattern patternname','整形パターン名指定') { |v| patternname = v }
-opt.on('-l',            '--littleend',          '入力ファイルの多バイトデータをlittle endianとして扱う') { endian = "little" }
-opt.on('-b',            '--bigend',             '入力ファイルの多バイトデータをbig endianとして扱う') { endian = "big" }
+  argv = opt.parse(ARGV)
 
-argv = opt.parse(ARGV)
-
-printf("inputfile\t= \"%s\"\n",inputfilename)
-printf("outputfile\t= \"%s\"\n",outputfilename)
-printf("formatfile\t= \"%s\"\n",formatfilename)
-printf("patternname\t= \"%s\"\n",patternname)
-
-# schema
-
-schema = YAML.load(schema_def)
-validator = Kwalify::Validator.new(schema)
-
-# format
-
-begin
-  f_file = File.read(formatfilename)
-rescue => ex
-  puts "Error: formatfile can not open"
-  printf("\t%s\n" ,ex.message)
-  exit 1
+  printf("inputfile\t= \"%s\"\n",$inputfilename)
+  printf("outputfile\t= \"%s\"\n",$outputfilename)
+  printf("formatfile\t= \"%s\"\n",$formatfilename)
+  printf("patternname\t= \"%s\"\n",$patternname)
 end
 
-yaml = ""
+# schema validation
+def format_schema_validation
+  schema = YAML.load($SCHEMA_DEF)
+  validator = Kwalify::Validator.new(schema)
 
-f_file.each_line do |line|
-  yaml << line.gsub(/([^\t]{8})|([^\t]*)\t/n) { [$+].pack("A8") }
-end
-
-yaml_data = YAML.load(yaml)
-
-errors = validator.validate(yaml_data)
-if !errors || errors.empty? then
-else
-  puts "Error: invalid format file"
-  errors.each do |error|
-    printf( "\t\"%s\" [%s}] %s\n",formatfilename,error.path,error.message)
+  begin
+    f_file = File.read($formatfilename)
+  rescue => ex
+    puts "Error: formatfile can not open"
+    printf("\t%s\n" ,ex.message)
+    exit 1
   end
-  exit(1)
-end
 
-yaml_data.each do |ptn|
-  if ptn["patternname"] == patternname then
-    pattern = ptn["format"]
+  yaml = ""
+
+  f_file.each_line do |line|
+    yaml << line.gsub(/([^\t]{8})|([^\t]*)\t/n) { [$+].pack("A8") }
   end
-end
 
-if pattern == nil then
-  puts "Error: pattern not found"
-  printf("\tpatternfile = \"%s\", patternname = \"%s\"\n",patternfilename,patternname)
-  exit(1)
-end
+  $yaml_data = YAML.load(yaml)
 
-header = []
-format = []
-
-pattern.each do |member|
-  case member["type"]
-  when *dummy
+  errors = validator.validate($yaml_data)
+  if !errors || errors.empty? then
   else
-    header.push member["label"]
-  end
-  format.push member["type"]
-end
-
-# convert
-
-begin
-  binary = File.binread(inputfilename)
-rescue => ex
-  puts "Error: inputfile can not open"
-  printf("\t%s\n" ,ex.message)
-  exit 1
-end
-
-begin
-  o_file = File.open(outputfilename,"w")
-rescue => ex
-  puts "Error: outputfile can not open"
-  printf("\t%s\n" ,ex.message)
-  exit 1
-end
-
-out_str = header.join("\t") + "\n"
-o_file.write out_str
-
-while binary.size > 0 do
-  str = []
-  
-  format.each do |fmt|
-    f = format_str[fmt]
-    template = f[endian]
-    data = binary.unpack(template)
-    num = data.pack(f["pack"]).unpack(f["unpack"])
-    case fmt
-    when *dummy
-    else
-      str.push sprintf(f["sprintf"], num[0])
+    puts "Error: invalid format file"
+    errors.each do |error|
+      printf( "\t\"%s\" [%s}] %s\n",$formatfilename,error.path,error.message)
     end
-    cut = data.pack(template)
-    binary2 = binary[cut.size..binary.size]
-    binary = binary2
+    exit(1)
   end
-  
-  out_str = str.join("\t") + "\n"
-  
-  o_file.write out_str
 end
 
-o_file.close
+def data_convert
+  option_parse
+  format_schema_validation
+
+  # pattern
+
+  $yaml_data.each do |ptn|
+    if ptn["patternname"] == $patternname then
+      $pattern = ptn["format"]
+    end
+  end
+
+  if $pattern == nil then
+    puts "Error: pattern not found"
+    printf("\tpatternfile = \"%s\", patternname = \"%s\"\n",patternfilename,$patternname)
+    exit(1)
+  end
+
+  header = []
+  format = []
+
+  $pattern.each do |member|
+    case member["type"]
+    when *$DUMMY
+    else
+      header.push member["label"]
+    end
+    format.push member["type"]
+  end
+
+  # convert
+
+  begin
+    binary = File.binread($inputfilename)
+  rescue => ex
+    puts "Error: inputfile can not open"
+    printf("\t%s\n" ,ex.message)
+    exit 1
+  end
+
+  begin
+    o_file = File.open($outputfilename,"w")
+  rescue => ex
+    puts "Error: outputfile can not open"
+    printf("\t%s\n" ,ex.message)
+    exit 1
+  end
+
+  out_str = header.join("\t") + "\n"
+  o_file.write out_str
+
+  while binary.size > 0 do
+    str = []
+    
+    format.each do |fmt|
+      f = $FORMAT_STR[fmt]
+      template = f[$endian]
+      data = binary.unpack(template)
+      num = data.pack(f["pack"]).unpack(f["unpack"])
+      case fmt
+      when *$DUMMY
+      else
+        str.push sprintf(f["sprintf"], num[0])
+      end
+      cut = data.pack(template)
+      binary2 = binary[cut.size..binary.size]
+      binary = binary2
+    end
+    
+    out_str = str.join("\t") + "\n"
+    
+    o_file.write out_str
+  end
+
+  o_file.close
+end
+
+
+
+data_convert
