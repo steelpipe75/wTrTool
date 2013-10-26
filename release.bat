8ee6c9dccf1d4ad50ddb0a6b89b54262fed3640a
Y.N <kazumiya50@hotmail.com>
2013-10-26 10:32:44 +0900

    ライセンスファイル、readmeファイルを追加するように修正

--- release.bat
+++ release.bat
@@ -1,3 +1,5 @@
 copy wTrTool.exe .\wTrTool\wTrTool.exe /Y
 copy wTrToolFormat.yaml .\wTrTool\wTrToolFormat.yaml /Y
 copy MemTrace.dat .\wTrTool\MemTrace.dat /Y
+copy MIT-LICENSE.txt .\wTrTool\MIT-LICENSE.txt /Y
+copy README.md .\wTrTool\README.md /Y
