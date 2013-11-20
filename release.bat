e49bad511882969d4ba808d4ba343fbcff855cab
Nishizaki <steelpipe75@gmail.com>
2013-11-20 19:00:37 +0900

    配布物にwTrToool.rbを追加

--- release.bat
+++ release.bat
@@ -3,3 +3,5 @@ copy wTrToolFormat.yaml .\wTrTool\wTrToolFormat.yaml /Y
 copy MemTrace.dat .\wTrTool\MemTrace.dat /Y
 copy MIT-LICENSE.txt .\wTrTool\MIT-LICENSE.txt /Y
 copy README.md .\wTrTool\README.md /Y
+copy wTrTool.rb .\wTrTool\wTrTool.rb /Y
+
